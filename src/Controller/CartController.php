4b8aac1f8bf5af630907edea4986dc70120a069c
JennyViannay <jenny.viannay75@gmail.com>
2020-10-22 21:35:26 +0200

    fix

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -66,6 +66,8 @@ class CartController extends AbstractController
 
     public function payment($infos)
     {
+        $stripe = \Stripe\Stripe::setApiKey(API_KEY);
+        
         $commandManager = new CommandManager();
         $data = [
             'name' => $infos['name'],
